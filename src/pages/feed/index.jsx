0bb7cd9aa22836d63d9703e8cf253b78f8d822f4
ErickDevUp <erickDevUp@gmail.com>
2023-02-07 09:11:25 +0100

    Add files via upload

---
+++ src/pages/feed/index.jsx
@@ -0,0 +1,26 @@
+import FeedVideos from "@/components/FeedVideos";
+import VideoPlayer from "@/components/VideoPlayer";
+import Head from "next/head";
+import Script from "next/script";
+
+import styles from "./styles.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.App}>
+      <Head>
+        <title>hShort</title>
+        <meta name="description" content="short video for hive" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      </Head>
+      <main className={styles.main}>
+        <FeedVideos />
+      </main>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/hls.js@latest"
+        strategy="afterInteractive"
+      ></Script>
+    </div>
+  );
+}
