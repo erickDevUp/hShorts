f98044a5bb7af44228a050817a31eb171128834e
Erick García <erickDevUp@gmail.com>
2023-02-07 22:04:57 +0100

    Add files via upload

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -7,10 +7,9 @@ import VideoPlayerActions from "./VideoPlayerActions.jsx";
 import VideoDescription from "../VideoDescription/index.jsx";
 //import videosrc from '/vid.mp4'
 import React from "react";
+import ReactPlayer from 'react-player/lazy'
 
-import dynamic from "next/dynamic";
 
-const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 export default function VideoPlayer({ playUrl, author, tags, title, images }) {
   const vidRef = useRef(null);
@@ -19,17 +18,24 @@ export default function VideoPlayer({ playUrl, author, tags, title, images }) {
   const handlePlay = () => {
     console.log(vidRef);
   };
+const [hasWindow, setHasWindow] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
 
   return (
     <div className={styles.wrapper}>
-      <ReactPlayer
+    {hasWindow ? <ReactPlayer
         className="react-player"
         url="https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
         width="100%"
         height="100%"
         ref={vidRef}
         autoPlay
-      />
+      />: "not found"}
 
       <i
         className={playVideo ? styles.player : styles.hidden}
