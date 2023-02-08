a53e2efae48e7ed6eed14b9c0ba3b392a10ca7ea
erickdevup <erickdevup@gmail.com>
2023-02-08 08:11:30 +0100

    muted fix

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { use, useEffect, useMemo, useRef, useState } from "react";
+import { use, useContext, useEffect, useMemo, useRef, useState } from "react";
 import styles from "./styles.module.css";
 import VideoPlayerActions from "./VideoPlayerActions.jsx";
 
@@ -7,41 +7,48 @@ import VideoPlayerActions from "./VideoPlayerActions.jsx";
 import VideoDescription from "../VideoDescription/index.jsx";
 //import videosrc from '/vid.mp4'
 import React from "react";
-import ReactPlayer from 'react-player/lazy'
-
-
+import ReactPlayer from "react-player/lazy";
+import nearUse from "@/hooks/nearUse";
+import audioCtx from "@/context/audioCtx";
 
 export default function VideoPlayer({ playUrl, author, tags, title, images }) {
   const vidRef = useRef(null);
-  const [playVideo, setPlayVideo] = useState(false);
+  const [hasWindow, setHasWindow] = useState(false);
 
-  const handlePlay = () => {
-    console.log(vidRef);
-  };
-const [hasWindow, setHasWindow] = useState(false);
+
+
+  const { muting, setMuting } = useContext(audioCtx);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       setHasWindow(true);
     }
   }, []);
-
+  const { handleMuted } = nearUse(vidRef);
   return (
     <div className={styles.wrapper}>
-    {hasWindow ? <ReactPlayer
-        className="react-player"
-        url={playUrl}
-        width="100%"
-        height="100%"
-        ref={vidRef} 
-        playing
-        loop
-        muted
-      />: "not found"}
+      {hasWindow ? (
+        <ReactPlayer
+          className="react-player"
+          url={playUrl}
+          width="100%"
+          height="100%"
+          ref={vidRef}
+          playing
+          loop
+          muted
+        />
+      ) : (
+        "not found"
+      )}
 
       <i
-        className={playVideo ? styles.player : styles.hidden}
-        onClick={handlePlay}
+       // className={playVideo ? styles.player : styles.hidden}
+       // onClick={handlePlay}
+      />
+      <i
+        className={muting ? styles.audioMuted : styles.audio}
+        onClick={handleMuted}
       />
       <VideoPlayerActions />
       <VideoDescription
--- src/hooks/nearUse.js
+++ src/hooks/nearUse.js
@@ -1,13 +1,11 @@
+import audioCtx from "@/context/audioCtx";
+import { useState, useEffect, useContext } from "react";
 
-import audioCtx from '@/context/audioCtx'
-import {useState, useEffect, useContext} from 'react'
-
-
-let observer
-const isClient = typeof window !== 'undefined'
+let observer;
+const isClient = typeof window !== "undefined";
 
 const hasIntersectionObserverSupport = () =>
-typeof window.IntersectionObserver !== 'undefined'
+  typeof window.IntersectionObserver !== "undefined";
 
 // import intersection observer polyfill only if on client
 // and if not supported by browser
@@ -15,38 +13,38 @@ const getIntersectionObserver = () => {
   return Promise.resolve(
     isClient &&
       !hasIntersectionObserverSupport() &&
-      import('intersection-observer')
-  )
-}
+      import("intersection-observer")
+  );
+};
 
 // options for intersection observer in order to improve the effect
 const intersectionObserverOptions = {
   root: null, // windows viewport will be used
-  rootMargin: '100px 0px 0px 0px'
-}
+  rootMargin: "0px",
+  threshold: 0.9,
+};
 
 const handleIntersect = (entries, observer) => {
-  entries
-    .filter(entry => entry.isIntersecting)
-    .forEach(entry => {
-      const {target} = entry
-      target._onIntersect(observer)
-    })
-}
+  entries.forEach((entry) => {
+    const { target, isIntersecting } = entry;
+    target._onIntersect(observer, isIntersecting);
+  });
+};
 
 // get observer from the cache or create a new one
 const getObserver = () =>
-  getIntersectionObserver().then(_ => {
-    if (observer) return observer
+  getIntersectionObserver().then((_) => {
+    if (observer) return observer;
     if (isClient) {
       return new window.IntersectionObserver(
         handleIntersect,
         intersectionObserverOptions
-      )
+      );
     }
-  })
+  });
 
-export default ({video}) => {
+
+export default (video) => {
 
   //  const [playing, setPlaying] = useState(false);
 
@@ -95,7 +93,7 @@ export default ({video}) => {
 
   const handleMuted = () => {
     const { current: videoEl } = video;
-    muting ? (videoEl.contentWindow.speaker[0].muted = false) : ( videoEl.contentWindow.speaker[0].muted = true);
+    muting ? (videoEl.muted = false) : ( videoEl.muted = true);
 
     setMuting(!muting);
   };
