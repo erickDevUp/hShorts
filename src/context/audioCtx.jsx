cf4cbe31b46fd3a9cc1e60d8eb557d5d8d4b5459
erickdevup <erickdevup@gmail.com>
2023-02-08 08:40:36 +0100

    playing fix err

--- src/context/audioCtx.jsx
+++ src/context/audioCtx.jsx
@@ -1,20 +1,4 @@
 
-import React, { useState } from 'react';
-
-
-const audioCtx = React.createContext({})
-export const AudioCtx = ({children}) => {
-
-
-    const [muting, setMuting] = useState(true);
-
-    return(<audioCtx.Provider value={{muting, setMuting}}>
-        {children}
-    </audioCtx.Provider>
-)
-}
- 
-export default audioCtx;
 
 import React, { useState } from 'react';
 
